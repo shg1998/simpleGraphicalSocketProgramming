eae480ca97412210e8bdb20d96b8014b56c3f930
shg 1998 <52446257+shg1998@users.noreply.github.com>
2020-06-12 22:26:50 +0430

    simple socket programming in C# winforms

---
+++ client/client/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+
+        string serverIP = "localhost";
+        int port = 1080;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpClient client = new TcpClient(serverIP, port);
+
+            int byteCount = Encoding.ASCII.GetByteCount(message.Text +1);
+
+            byte[] sendBfr = new byte[byteCount];
+            sendBfr = Encoding.ASCII.GetBytes(message.Text +";");
+
+            NetworkStream stream = client.GetStream();
+            stream.Write(sendBfr,0,sendBfr.Length);
+
+            stream.Close();
+            client.Close();
+
+        }
+    }
+}
---
+++ socketProgramming/socketProgramming/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace socketProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+            TcpListener server = new TcpListener(ip, 1080);
+            TcpClient client = default(TcpClient);
+            try
+            {
+                server.Start();
+                Console.WriteLine("server started ...");
+                Console.Read();
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.Read();
+            }
+
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+                byte[] recivedBfr = new byte[100];
+                NetworkStream stream = client.GetStream();
+
+                stream.Read(recivedBfr, 0, recivedBfr.Length);
+
+                StringBuilder ms = new StringBuilder();
+                foreach (byte b in recivedBfr)
+                {
+                    if (b.Equals(00))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        ms.Append(Convert.ToChar(b).ToString());
+                    }
+                }
+                Console.WriteLine(ms.ToString() + ms.Length);
+                Console.Read();
+            }
+        }
+    }
+}
